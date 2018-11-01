dca55e06b66af3975b68418f21ff830d658cdbff
wise-bit <44659197+wise-bit@users.noreply.github.com>
2018-10-31 22:47:40 -0400

    Created program #1 for GCI
    Prints a message 10 times, asks for a name, and prints a sentence along with the name reversed.

---
+++ program1.py
@@ -0,0 +1,5 @@
+for i in range(10):
+	print("GCI is great!")
+
+s = input("Please input your name: ")
+print("Hello {}, pleased to meet you! Fun fact, your name reversed is \"{}\"".format(s, s[::-1]))
